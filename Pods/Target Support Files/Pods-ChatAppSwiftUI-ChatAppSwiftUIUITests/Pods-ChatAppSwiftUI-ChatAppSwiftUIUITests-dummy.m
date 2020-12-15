087edddc2469a33a961a668a4fbe82fb2e445a46
mohammed <mohamed.alefrangy@gmail.com>
2020-12-15 22:55:58 +0200

    Add chatApp workspase

---
+++ Pods/Target Support Files/Pods-ChatAppSwiftUI-ChatAppSwiftUIUITests/Pods-ChatAppSwiftUI-ChatAppSwiftUIUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ChatAppSwiftUI_ChatAppSwiftUIUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ChatAppSwiftUI_ChatAppSwiftUIUITests
+@end
